b19be08cf126ed899bbd565f2c9de85064b38c2a
mrobinson32-gomu <mrobinson32@dmacc.edu>
2026-05-02 18:33:48 -0500

    updated with progress check code

---
+++ Driver/FinalProjectRobinson.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FinalProjectRobinson {
+    public static String capitalize(String text) {
+        return text.substring(0, 1).toUpperCase() + text.substring(1);
+    }
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        ArrayList<String> watchlist = new ArrayList<>();
+
+        System.out.println("Do you want to create a TV show or movie watchlist?");
+        String choice = scnr.nextLine().toLowerCase();
+
+        while(!choice.equals("movies") && !choice.equals("tv shows")) {
+            System.out.println("Please enter 'movies or 'tv shows':");
+            choice = scnr.nextLine().toLowerCase();
+        }
+
+        System.out.println("Enter your " + choice + ". Type 'end list' to finish.");
+
+        while (true) {
+            String item = scnr.nextLine();
+            if (item.equalsIgnoreCase("end list")) {
+                break;
+            }
+            watchlist.add(item);
+        }
+
+        System.out.println("\n" + capitalize(choice) + " watchlist:");
+        for (String item : watchlist) {
+            System.out.println("- " + item);
+        }
+        scnr.close();
+    }
+}
