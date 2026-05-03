5988eb601f3ad669e1929cca0fb72d96b7f16a2b
mrobinson32-gomu <mrobinson32@dmacc.edu>
2026-05-03 17:28:02 -0500

    Project structure

--- Driver/WatchlistDriver.java
+++ Driver/WatchlistDriver.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class FinalProjectRobinson {
+public class WatchlistDriver {
     public static String capitalize(String text) {
         return text.substring(0, 1).toUpperCase() + text.substring(1);
     }
